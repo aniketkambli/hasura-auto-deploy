27129d34f9bb58d25f2db02c455e019729631b17
Aniket Kambli <38850180+aniketkambli@users.noreply.github.com>
2023-04-14 14:32:20 +0530

    Demo branch (#5)
    * update compose file
    
    * add new table
    
    * add third table
    
    * add 4
    
    * update workflow file
    
    * add a new column
    
    ---------
    
    Co-authored-by: udaniketk <aniket.kambli@udgama.com>

---
+++ migrations/default/1681462886041_alter_table_public_test_4_add_column_new_col/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."test_4" add column "new_col" integer
+--  null;
